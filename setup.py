e280e15034694fe5c79790b4fd4854991a042b09
Silas Ray <silas.ray@gmail.com>
2014-04-02 14:17:44 -0400

    Changed package name
    Changed to nose-sneeze to avoid conflict with existing sneeze package in PyPI.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='sneeze',
+setup(name='nose-sneeze',
       version='0.0.0',
       packages=find_packages(),
       install_requires=['SQLAlchemy',
