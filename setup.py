44d106c7152e67ad6e5e70355283e001cb79380f
Silas Ray <silas.ray@gmail.com>
2014-04-02 14:30:45 -0400

    Fleshed out setup.py
    Added fields in preparation for PyPI submission

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(name='nose-sneeze',
-      version='0.0.0',
+      version='0.0.1',
+      author='Silas Ray',
+      author_email='silas.ray@nytimes.com',
+      license='Apache2.0',
+      url='http://sneeze.readthedocs.org/',
+      description='A nose plugin for better reporting',
+      long_description='A nose plugin that provides a platform to facilitate better result reporting.',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Information Technology',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Software Development :: Testing']
       packages=find_packages(),
       install_requires=['SQLAlchemy',
                         'nose >= 1.2',
